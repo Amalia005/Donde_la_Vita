3ba129743d986984f207042397de0d7da759b72d
Amalia005 <amalia.toledo@estudiantes.uv.cl>
2026-05-22 20:12:03 -0400

    estructura inicial

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+// App.js - Enrutador entre vistas
+import React from 'react';
+// import Garzon from './views/Garzon/Garzon';
+// import Cocina from './views/Cocina/Cocina';
+// import Admin from './views/Admin/Admin';
+
+function App() {
+  return (
+    <div>
+      <h1>Donde la Vita</h1>
+      {/* Aquí irá el enrutador entre vistas */}
+    </div>
+  );
+}
+
+export default App;
